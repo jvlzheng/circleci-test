e11db9d01fc1f57338403ec7b5cea5e8b9a585de
Michelle Zheng <michelle.zheng@mzheng-ltm.internal.salesforce.com>
2020-06-02 17:25:48 -0500

    phpcs

--- .circleci/phpcs.sh
+++ .circleci/phpcs.sh
@@ -49,11 +49,14 @@ fi
 echo "Grabbing WordPress Coding Standards"
 git clone -q -b master https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wpcs
 
+echo "Getting phpcs"
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+
 echo "Adding WPCS to phpcs path"
-phpcs --config-set installed_paths $(pwd)/wpcs
+./vendor/bin/phpcs --config-set installed_paths $(pwd)/wpcs
 
 echo "Checking installed paths"
-phpcs -i
+./vendor/bin/phpcs -i
 
 echo "Running phpcs..."
-phpcs $changed_files
+./vendor/bin/phpcs $changed_files
